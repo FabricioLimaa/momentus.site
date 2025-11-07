d103ed042ada1a042626be8561f5f522d878e28c
Fabricio Lima <fabriciodacostalima@gmail.com>
2025-11-07 17:57:56 -0300

    Atualiza configuração do projeto: remove script de postinstall, adiciona interface de ambiente e cria configuração de depuração para o VSCode

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,5 +1,10 @@
 import { Hono } from "hono";
 
+interface Env {
+  SOME_BINDING: string; // Exemplo de propriedade
+  ANOTHER_BINDING: number; // Adicione as propriedades necessárias
+}
+
 const app = new Hono<{ Bindings: Env }>();
 
 export default app;
